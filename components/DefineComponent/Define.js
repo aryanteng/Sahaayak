538e845b8e0bc3d2aa30ca69642e11470574fdfd
aryanteng <aryanteng892@gmail.com>
2022-04-11 15:01:20 +0530

    footer  done

---
+++ components/DefineComponent/Define.js
@@ -0,0 +1,8 @@
+import React from "react";
+import styles from "./styles.module.css";
+
+function Define() {
+  return <div>Define</div>;
+}
+
+export default Define;
--- components/Footer/Footer.js
+++ components/Footer/Footer.js
@@ -2,7 +2,21 @@ import React from "react";
 import styles from "./styles.module.css";
 
 function Footer() {
-  return <div className={styles.container}>Footer</div>;
+  const date = new Date();
+  let year = date.getFullYear();
+  return (
+    <div className={styles.container}>
+      <div className={styles.info}>
+        <h1>Sahaayak</h1>
+        <a>
+          <p>About Us</p>
+        </a>
+      </div>
+      <div style={{ marginTop: "-1rem" }}>
+        <p>Sahaayak Project ©{year}</p>
+      </div>
+    </div>
+  );
 }
 
 export default Footer;
