a0d5e5bacdbfc5caadfaf54670a5ca7d8e59d8fe
avivashishta29 <avivashishta@gmail.com>
2022-04-11 16:16:44 +0530

    footer

--- components/Footer/Footer.js
+++ components/Footer/Footer.js
@@ -1,7 +1,6 @@
 import Link from "next/link";
 import React from "react";
 import styles from "./styles.module.css";
-import { motion } from "framer-motion";
 
 function Footer() {
   const date = new Date();
@@ -10,33 +9,15 @@ function Footer() {
     <div className={styles.container}>
       <div className={styles.wrapper}>
         <div className={styles.info}>
-          <motion.h1
-            initial={{ x: 50, opacity: 0 }}
-            whileInView={{ x: 0, opacity: 1 }}
-            transition={{ delay: 0.75, duration: 0.5, type: "spring" }}
-          >
-            Sahaayak
-          </motion.h1>
+          <h1>Sahaayak</h1>
 
           <Link href="about-us" passHref={true}>
-            <motion.p
-              initial={{ x: 50, opacity: 0 }}
-              whileInView={{ x: 0, opacity: 1 }}
-              transition={{ delay: 0.75, duration: 0.5, type: "spring" }}
-              className={styles.link}
-            >
-              About Us
-            </motion.p>
+            <p className={styles.link}>About Us</p>
           </Link>
         </div>
-        <motion.div
-          initial={{ x: -50, opacity: 0 }}
-          whileInView={{ x: 0, opacity: 1 }}
-          transition={{ delay: 0.5, duration: 0.75, type: "spring" }}
-          style={{ marginTop: -1 + "rem", marginLeft: 1 + "rem" }}
-        >
+        <div style={{ marginTop: -1 + "rem", marginLeft: 1 + "rem" }}>
           <p>Sahaayak Project ©{year}</p>
-        </motion.div>
+        </div>
       </div>
     </div>
   );
