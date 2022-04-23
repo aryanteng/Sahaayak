265569009280594c749db2a47e931ee4810177b3
avivashishta29 <avivashishta@gmail.com>
2022-04-23 22:03:24 +0530

    mouse animation

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,30 @@
-import '../styles/globals.css'
+import { useEffect } from "react";
+import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  var cursor;
+  var cursor2;
+
+  useEffect(() => {
+    cursor = document.getElementById("cursor");
+    document.body.addEventListener("mousemove", function (e) {
+      (cursor.style.left = e.clientX + "px"),
+        (cursor.style.top = e.clientY + "px");
+    });
+    cursor2 = document.getElementById("cursor2");
+    document.body.addEventListener("mousemove", function (e) {
+      (cursor2.style.left = e.clientX + "px"),
+        (cursor2.style.top = e.clientY + "px");
+    });
+  }, []);
+
+  return (
+    <>
+      <div className="cursor2" id="cursor2"></div>
+      <div className="cursor" id="cursor"></div>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
