9b3ae226ebe1e2a02b21736e12bee23378ce4a57
avivashishta29 <avivashishta@gmail.com>
2022-04-23 23:05:12 +0530

    orfs and download done

--- components/DnaComponents/InputDNA/OutputDNA.js
+++ components/DnaComponents/InputDNA/OutputDNA.js
@@ -1,13 +1,31 @@
 import React from "react";
 import styles from "./styles.module.css";
 
-function OutputDNA() {
+function OutputDNA({ isOrf }) {
+  const downloadTxtFile = () => {
+    const element = document.createElement("a");
+    const file = new Blob(["hello world"], {
+      type: "text/plain",
+    });
+    element.href = URL.createObjectURL(file);
+    element.download = isOrf ? "orf.txt" : "protein.txt";
+    document.body.appendChild(element);
+    element.click();
+  };
+
   return (
     <div className={styles.container}>
       <div className={styles.infoBox}>
         <h1>Output</h1>
         <textarea disabled className={styles.input} />
-        <div className={styles.btn}>Download</div>
+        <div
+          className={styles.btn}
+          onClick={() => {
+            downloadTxtFile();
+          }}
+        >
+          Download
+        </div>
       </div>
       <img className={styles.img} src="assets/Output.svg" width="150"></img>
     </div>
---
+++ pages/orfs.js
@@ -0,0 +1,33 @@
+import { useState } from "react";
+import Define from "../components/DefineComponent/Define";
+import InputDNA from "../components/DnaComponents/InputDNA";
+import OutputDNA from "../components/DnaComponents/InputDNA/OutputDNA";
+import Footer from "../components/Footer/Footer";
+import Header from "../components/Header/Header";
+import styles from "../styles/Global.module.css";
+
+export default function ORFS() {
+  const topic = "ORFS";
+  const definition =
+    "An open reading frame is a portion of a DNA molecule that, when translated into amino acids, contains no stop codons.<br/><br/> The genetic code reads DNA sequences in groups of three base pairs, which means that a double-stranded DNA molecule can read in any of six possible reading frames--three in the forward direction and three in the reverse. A long open reading frame is likely part of a gene.";
+  //   LINK AND CONTENT PLEASE SEE OF ORFS
+  const link = "https://vlab.amrita.edu/?sub=3&brch=274&sim=1431&cnt=1";
+
+  const [seq, setSeq] = useState("");
+
+  const submit = () => {
+    console.log("ans", ans);
+  };
+
+  return (
+    <>
+      <Header />
+      <div className={styles.container}>
+        <Define topic={topic} definition={definition} link={link} />
+        <InputDNA seq={seq} setSeq={setSeq} submit={submit} />
+        <OutputDNA isOrf={true} />
+      </div>
+      <Footer />
+    </>
+  );
+}
